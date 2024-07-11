81d9cab51150acafe08369416e50f216ab90a2ba
José Cavalcante <joseluismoreiracavalcante@gmail.com>
2024-07-11 15:27:42 -0300

    Melhorias gerais e Seção de programação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LandingPageComponent } from './landing-page/landing-page.component';
+import { PageComponent } from './page/page.component';
 
 const routes: Routes = [
-  {path: '', component: LandingPageComponent}
+  { path: '', redirectTo: '/page', pathMatch: 'full' },
+  { path: 'page', component: PageComponent }
 ];
 
 @NgModule({
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { AppRoutingModule } from './app/app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { bootstrapApplication } from '@angular/platform-browser';
 
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(BrowserModule, AppRoutingModule, BrowserAnimationsModule)
+  ]
+}).catch(err => console.error(err));
