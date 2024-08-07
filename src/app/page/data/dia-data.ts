a4fa8d0a42ac8a4568ebaec022df1446db5e24d4
José Cavalcante <joseluismoreiracavalcante@gmail.com>
2024-08-07 15:03:21 -0300

    Atualizar programação do evento

--- src/app/page/data/dia-data.ts
+++ src/app/page/data/dia-data.ts
@@ -1,114 +1,156 @@
-import { Dia } from "../models/dia";
+import { Dia } from '../models/dia';
 
 export const diasData: Dia[] = [
   {
-    label: 'PRIMEIRO DIA',
+    label: 'DIA 05 | QUINTA-FEIRA',
     palestras: [
       {
-        horario: '00:00 - 00:00',
+        horario: '09:00 - 09:30',
         titulo: 'Registro e recepção dos participantes',
+        palestrante: '',
         participantes: [],
       },
       {
-        horario: '00:00 - 00:00',
+        horario: '09:30 - 09:50',
         titulo: 'Abertura Oficial do Fórum',
+        palestrante: '',
         participantes: [],
       },
       {
-        horario: '00:00 - 00:00',
-        titulo: 'O tema da palestra estará disponível em breve',
-        palestrante: 'Nome do palestrante',
+        horario: '10:00 - 10:30',
+        titulo: 'Palestra do Corregedor Geral da Justiça',
+        palestrante: 'Desembargador Olímpio Galvão',
         participantes: [],
       },
       {
-        horario: '00:00 - 00:00',
-        titulo: 'O tema do painel estará disponível em breve',
+        horario: '10:30 - 11:30',
+        titulo: 'Palestra de Abertura: Inovação, você está pronto?',
+        palestrante: 'Dr. Guto Niche',
+        participantes: [],
+      },
+      {
+        horario: '14:00 - 15:00',
+        titulo:
+          'Painel - Ética, Segurança e Privacidade no Uso de Tecnologias no Âmbito Jurídico',
+        palestrante: '',
         participantes: [
-          'Nome do participante 01',
-          'Nome do participante 02',
-          'Nome do participante 03',
+          'Clayton Athaíde (Mediador)',
+          'Dr. Nazareno Reis',
+          'Dr. Vinício Rosal Júnior',
         ],
       },
       {
-        horario: '00:00 - 00:00',
-        titulo: 'O tema da palestra estará disponível em breve',
-        palestrante: 'Nome do palestrante',
+        horario: '15:00 - 15:50',
+        titulo:
+          'Palestra - Blockchain e a imutabilidade de dados: Garantindo a autenticidade e a confiabilidade de atos jurídicos',
+        palestrante: 'Dr. Thiago Aleluia',
         participantes: [],
       },
       {
-        horario: '00:00 - 00:00',
-        titulo: 'O tema da palestra estará disponível em breve',
-        palestrante: 'Nome do palestrante',
+        horario: '15:50 - 16:10',
+        titulo: 'Intervalo',
+        palestrante: '',
+        participantes: [],
+      },
+      {
+        horario: '16:10 - 17:10',
+        titulo:
+          'Palestra: Como liderar com propósito e Impacto na Era da IA: os desafios e as oportunidades para os líderes do futuro',
+        palestrante: 'Leandro Matos',
+        participantes: [],
+      },
+      {
+        horario: '17:30 - 18:30',
+        titulo: 'Happy Hour (Música e Coquetel)',
+        palestrante: '',
         participantes: [],
       },
     ],
   },
   {
-    label: 'SEGUNDO DIA',
+    label: 'DIA 06 | SEXTA-FEIRA',
     palestras: [
       {
-        horario: '00:00',
+        horario: '08:30',
         titulo: 'Abertura',
         participantes: [],
       },
       {
-        horario: '00:00 - 00:00',
-        titulo: 'O tema do painel estará disponível em breve',
+        horario: '08:50 - 09:30',
+        titulo: 'Painel - Tecnologias Emergentes e Disrupção no Judiciário',
         participantes: [
-          'Nome do participante 01',
-          'Nome do participante 02',
-          'Nome do participante 03',
+          'Priscila Dantas (Mediadora)',
+          'Dr. Marcelo Mesquita (Juiz do TJ)',
+          'Dr. Maurício Machado (Presidente da AMAPI)',
         ],
       },
       {
-        horario: '00:00 - 00:00',
-        titulo: 'O tema da palestra estará disponível em breve',
-        palestrante: 'Nome do palestrante',
+        horario: '09:30 - 10:00',
+        titulo:
+          'Palestra - Impacto das Inovações Tecnológicas no Direito Eleitoral',
+        palestrante: 'Dr. Horácio Neiva',
         participantes: [],
       },
       {
-        horario: '00:00 - 00:00',
-        titulo: 'O tema do painel estará disponível em breve',
-        participantes: [
-          'Nome do participante 01',
-          'Nome do participante 02',
-          'Nome do participante 03',
-        ],
-      },
-      {
-        horario: '00:00 - 00:00',
-        titulo: 'O tema da palestra estará disponível em breve',
-        palestrante: 'Nome do palestrante',
+        horario: '10:00 - 10:20',
+        titulo: 'Intervalo',
         participantes: [],
       },
       {
-        horario: '00:00 - 00:00',
-        titulo: 'O tema da palestra estará disponível em breve',
-        palestrante: 'Nome do palestrante',
+        horario: '10:20 - 11:00',
+        titulo:
+          'Painel - Inovação Aberta e Colaboração: O papel da colaboração entre empresas, startups, universidades e governos no estímulo à inovação',
+        palestrante: '',
+        participantes: [
+          'Gov. Rafael Fonteles',
+          'Ellen Gera (Presidente da ETIPI)',
+          'André Macedo (Gestor da Secretaria de Inteligência Artificial)',
+        ],
+      },
+      {
+        horario: '11:00 - 12:00',
+        titulo: 'Palestra - Quais são as novas habilidades do futuro?',
+        palestrante: 'Guga Stocco',
         participantes: [],
       },
       {
-        horario: '00:00 - 00:00',
-        titulo: 'O tema do painel estará disponível em breve',
+        horario: '14:30 - 15:30',
+        titulo: 'Painel - Tecnologias Emergentes e o Futuro da Advocacia',
         participantes: [
-          'Nome do participante 01',
-          'Nome do participante 02',
-          'Nome do participante 03',
+          'Prof. Alana (Mediadora)',
+          'Dr. Celso Barros (Presidente da OAB)',
+          'Dra. Giovana Nunes',
         ],
       },
       {
-        horario: '00:00 - 00:00',
-        titulo: 'O tema do painel estará disponível em breve',
+        horario: '15:30 - 16:00',
+        titulo: 'Coffee Break',
+        participantes: [],
+      },
+      {
+        horario: '16:00 - 16:50',
+        titulo: 'Painel - Inovação e Educação Jurídica',
         participantes: [
-          'Nome do participante 01',
-          'Nome do participante 02',
-          'Nome do participante 03',
+          'Dr. Dimmy Magalhães (Mediador)',
+          'Des. José Wilson',
+          'Dra. Melissa (Juíza)'
         ],
       },
       {
-        horario: '00:00 - 00:00',
-        titulo: 'O tema da palestra estará disponível em breve',
-        palestrante: 'Nome do palestrante',
+        horario: '16:50 - 17:20',
+        titulo: 'Apresentação dos Projetos desenvolvidos na Oficina de Design Thinking',
+        participantes: [],
+      },
+      {
+        horario: '17:30 - 18:30',
+        titulo: 'Palestra - Tema a definir',
+        palestrante: 'João Kepler',
+        participantes: [],
+      },
+      {
+        horario: '18:30',
+        titulo: 'Encerramento com Apresentação do Projeto Vencedor e Assinatura do Termo de Cooperação minutado na Oficina',
+        palestrante: '',
         participantes: [],
       },
     ],
